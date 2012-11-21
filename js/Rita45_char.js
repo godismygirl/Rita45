8513b380b01cea468c26f65750d0d2923e55bd64
Jin <godismygirl@gmail.com>
2012-11-22 00:37:06 +0800

    char data added

--- js/Rita45_char.js
+++ js/Rita45_char.js
@@ -1,3 +1,71 @@
+function loadChar(){
+
+}
+
+var mapChar = {
+	SienaGorge : {
+		ally : [
+			{
+				race : 'aivera',
+				job : 'sniper',
+				name : 'JK_Rose',
+				position : [13,4],
+				level : 5,
+				equip_01 : ['WE001'],
+				equip_02 : ['AR001'],
+				equip_03 : ['SH001'],
+				equip_04 : ['AC002'],
+				equip_05 : ['AC001'],
+				item : ['IT001','IT002'],
+				skill : ['SK001','SK002']
+			},
+
+			{
+				race : 'aivera',
+				job : 'unique',
+				name : 'Shara',
+				position : [13,5],
+				level : 4,
+				equip_01 : ['WE001'],
+				equip_02 : ['AR001'],
+				equip_03 : ['SH001'],
+				equip_04 : ['AC002'],
+				equip_05 : ['AC001'],
+				item : ['IT001','IT002'],
+				skill : ['SK001','SK002']
+			}
+		],
+
+		enemy : [
+			{
+				race : 'malboro',
+				job : 'monster',
+				name : 'Tata',
+				position : [2,4],
+				level : 5,
+				skill : ['SK001','SK002']
+			},
+
+			{
+				race : 'malboro',
+				job : 'monster',
+				name : 'Tata',
+				position : [1,4],
+				level : 5,
+				skill : ['SK001','SK002']
+			},
+
+			{
+				race : 'malboro',
+				job : 'monster',
+				name : 'Tata',
+				position : [0,6],
+				level : 8,
+				skill : ['SK001','SK002']
+			}
+		]
+	}
+}
 var sniper = {
 	src : 'url(images/sniper.png)',
 }
